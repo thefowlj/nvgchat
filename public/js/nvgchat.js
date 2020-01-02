41fa2d030bca6434250efa4c87ae322a5ba20455
jonfowler <jfowler27@gmail.com>
2020-01-01 17:26:19 -0700

    Update interface and stage for backend updates
    - Update index.html to organize interface more consistently
    
    - Update nvgchat.css with interface style changes
    
    - Update nvgchat.js to automatically keep chat box at the bottom
      of the screen
    
    - Update server.js to use JSON objects for messages and users

--- public/js/nvgchat.js
+++ public/js/nvgchat.js
@@ -3,31 +3,65 @@ NVG Chat
 nvgchat.js
 */
 var uname;
+var uColor = 'null';
 
 $(document).ready(() => {
   var socket = io();
 
-  $('.chatForm').hide();
+  uColor = getRandomHexColor();
+  console.log(uColor);
+
+  $('#messagesContainer').hide();
 
   $('#username').on('submit', function(e) {
     e.preventDefault();
     uname = $('#un').val();
     $('#initial').hide();
-    $('#messages').show();
-    $('.chatForm').show();
-    $('#m').focus();
+    //$('#messagesContainer').show();
+    //$('.chatForm').show();
+
     $('#chatUsername').text(uname);
+    $('#chatUsername').css('color', uColor);
+    $('#messagesContainer').css('display', 'flex');
+    $('#messagesContainer').css('height', '100%');
+
+    var newUser = { uname: uname, color: uColor };
+    socket.emit('newUser', newUser);
+    $(document).scrollTop($(document).height());
+    $('#m').focus();
     return false;
   });
 
   $('.chatForm').submit(function(e){
     e.preventDefault();
-    socket.emit('chat message', uname + ': ' + $('#m').val());
+    socket.emit(
+      'chat message',
+      {
+        uname: uname,
+        color: uColor,
+        message: $('#m').val()
+      }
+    );
     $('#m').val('');
     return false;
   });
   socket.on('chat message', function(msg){
-    console.log(msg);
-    $('#messages').prepend($('<li>').text(msg));
+    var msgText = msg.message;
+    var color = msg.color;
+    var uname = msg.uname;
+    $('#messages').append(`<li><span style='color: ${color}'>${uname}: </span>${msgText}</li>`);
+    $(document).scrollTop($(document).height());
   });
 });
+
+// https://stackoverflow.com/questions/1152024/best-way-to-generate-a-random-color-in-javascript/14187677#14187677
+function getRandomHexColor(brightness){
+  brightness = brightness == undefined ? 75 : brightness;
+  function randomChannel(brightness){
+    var r = 255-brightness;
+    var n = 0|((Math.random() * r) + brightness);
+    var s = n.toString(16);
+    return (s.length==1) ? '0'+s : s;
+  }
+  return '#' + randomChannel(brightness) + randomChannel(brightness) + randomChannel(brightness);
+}
--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const server = express();
 const http = require('http').createServer(server);
 const io = require('socket.io')(http);
 const dotenv = require('dotenv');
+const db = require('diskdb');
 
 dotenv.config();
-
 const port = process.env.PORT;
 
 server.use(express.static('public'));
+
+db.connect('db', ['currentUsers','messages']);
 
 server.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -24,6 +26,11 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
     io.emit('chat message', msg);
+  });
+  socket.on('newUser', (dataObj) => {
+    newUser = db.currentUsers.save(dataObj);
+    console.log(newUser);
+    return newUser._id;
   });
 });
 
