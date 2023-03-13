5359d6975bfdb8c5213f59b52087390021aa9829
Jon Fowler <jfowler27@gmail.com>
2023-03-13 10:57:36 -0700

    Add conversational logic to Python ChatGPT client

--- client/chatbot/python/chatgpt/nvg-chatbot.py
+++ client/chatbot/python/chatgpt/nvg-chatbot.py
@@ -9,14 +9,17 @@ import re
 import openai
 from dotenv import load_dotenv
 import os
+from distutils.util import strtobool
 
 load_dotenv()
 
-chatbot_username = os.getenv('CHATBOT_USERNAME') if os.getenv('CHATBOT_USERNAME') != None else 'Gregbot'
-regex = f'^.*(chatbot_username)'
+chatbot_username = os.getenv('CHATBOT_USERNAME', 'Gregbot')
+regex = f'^.*({chatbot_username})'
 uid = None
 openai.api_key = os.getenv('CHATGPT_API_KEY')
 un_color = '#000000'
+conversational = bool(strtobool(os.getenv('CHATBOT_CONVERSATIONAL', 'False')))
+conversations = {}
 
 # Adapted from the OpenAPI example: https://platform.openai.com/examples/default-marv-sarcastic-chat
 default_prompt = f'{chatbot_username} is a chatbot that reluctantly answers questions with sarcastic responses'
@@ -24,6 +27,14 @@ chatbot_prompt = os.getenv('CHATBOT_PROMPT') if os.getenv('CHATBOT_PROMPT') != N
 chatgpt_model = os.getenv('CHATGPT_MODEL') if os.getenv('CHATGPT_MODEL') != None else 'text-davinci-003'
 
 sio = socketio.AsyncClient()
+
+
+def check_key(dic, key):
+    keys_list = list(dic.keys())
+    output = False
+    if keys_list.count(key) == 1:
+        output = True
+    return output
 
 
 @sio.event
@@ -35,13 +46,21 @@ async def connect():
 
 @sio.on('chat message')
 async def chat_message(data):
+    global conversations
+    global uid
     message = data['message']
     query = ''
-    if chatbot_username in message and message != f'{chatbot_username} has entered the chat.':
-        query = re.sub(regex, '', message)
+    if (chatbot_username in message or (conversational and check_key(conversations, data['uid']))) and data['type'] == 'msg' and data['uid'] != uid:
+        if not(check_key(conversations, data['uid'])) and conversational:
+            prompt = f'{chatbot_prompt}:\n\nYou: {message}\n{chatbot_username}:'
+            conversations[data['uid']] = prompt
+        elif conversational:
+            prompt = conversations[data['uid']]
+            prompt = f'{prompt}\nYou: {message}\n{chatbot_username}:'
+            conversations[data['uid']] = prompt
         msg = openai.Completion.create(
             model=chatgpt_model,
-            prompt=f"{chatbot_prompt}:\n\nYou: {query}\nMarv:",
+            prompt=prompt,
             temperature=0.5,
             max_tokens=60,
             top_p=0.3,
@@ -55,6 +74,10 @@ async def chat_message(data):
             'type': 'msg',
             'message': msg.choices[0].text
         }
+        if conversational:
+            current_text = conversations[data['uid']]
+            conversations[data['uid']] = f'{current_text}{msg.choices[0].text}'
+            print(conversations[data['uid']])
         await sio.emit('chat message', response)
 
 
